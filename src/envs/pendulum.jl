09fe01de802cf1cd802d86a7546d9dd9b81a3cde
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-12-01 20:34:27 +0800

    make reset! return env (#30)
    - update doc

--- src/envs/pendulum.jl
+++ src/envs/pendulum.jl
@@ -8,49 +8,49 @@ angle_normalize(x) = ((x+π) % (2π)) - π
 
 
 mutable struct Pendulum <: AbstractEnvironment
-	θs::Tuple{Float64,Float64}
-	reward::Float64
-	a::Float64 # last action for rendering
-	steps::Int
-	maxsteps::Int
+  θs::Tuple{Float64,Float64}
+  reward::Float64
+  a::Float64 # last action for rendering
+  steps::Int
+  maxsteps::Int
 end
 Pendulum(maxsteps=500) = Pendulum((0.,0.),0.,0.,0,maxsteps)
 
 function reset!(env::Pendulum)
-	env.θs = (rand(Uniform(-π, π)), rand(Uniform(-1., 1.)))
-	env.reward = 0.0
-	env.a = 0.0
-	env.steps = 0
-	return
+  env.θs = (rand(Uniform(-π, π)), rand(Uniform(-1., 1.)))
+  env.reward = 0.0
+  env.a = 0.0
+  env.steps = 0
+  env
 end
 
 actions(env::Pendulum, s) = IntervalSet(-max_torque, max_torque)
 
 function step!(env::Pendulum, s, a)
-	θ, θvel = env.θs
-	g = 10.0
-	m = 1.0
-	l = 1.0
-	dt = 0.05
+  θ, θvel = env.θs
+  g = 10.0
+  m = 1.0
+  l = 1.0
+  dt = 0.05
 
-	env.a = a
-	a = clamp(a, -max_torque, max_torque)
-	env.reward = -(angle_normalize(θ)^2 + 0.1θvel^2 + 0.001a^2)
+  env.a = a
+  a = clamp(a, -max_torque, max_torque)
+  env.reward = -(angle_normalize(θ)^2 + 0.1θvel^2 + 0.001a^2)
 
-	# update state
-	newθvel = θvel + (-1.5g/l * sin(θ+π) + 3/(m*l^2)*a) * dt
-	newθ = θ + newθvel * dt
-	newθvel = clamp(newθvel, -max_speed, max_speed)
-	env.θs = newθ, newθvel
+  # update state
+  newθvel = θvel + (-1.5g/l * sin(θ+π) + 3/(m*l^2)*a) * dt
+  newθ = θ + newθvel * dt
+  newθvel = clamp(newθvel, -max_speed, max_speed)
+  env.θs = newθ, newθvel
 
-	env.steps += 1
-	env.reward, state(env)
+  env.steps += 1
+  env.reward, state(env)
 end
 
 
 function state(env::Pendulum)
-	θ, θvel = env.θs
-	Float64[cos(θ), sin(θ), θvel]
+  θ, θvel = env.θs
+  Float64[cos(θ), sin(θ), θvel]
 end
 
 finished(env::Pendulum, s′) = env.steps >= env.maxsteps
@@ -58,29 +58,29 @@ finished(env::Pendulum, s′) = env.steps >= env.maxsteps
 # ------------------------------------------------------------------------
 
 @recipe function f(env::Pendulum)
-	legend := false
-    xlims := (-1,1)
-    ylims := (-1,1)
-    grid := false
-    ticks := nothing
+  legend := false
+  xlims := (-1,1)
+  ylims := (-1,1)
+  grid := false
+  ticks := nothing
 
 	# pole
-	@series begin
-		w = 0.2
-		x = [-w,w,w,-w]
-		y = [-.1,-.1,1,1]
-		θ = env.θs[1]
-		fillcolor := :red
-		seriestype := :shape
-		x*cos(θ) - y*sin(θ), y*cos(θ) + x*sin(θ)
-	end
+  @series begin
+    w = 0.2
+    x = [-w,w,w,-w]
+    y = [-.1,-.1,1,1]
+    θ = env.θs[1]
+    fillcolor := :red
+    seriestype := :shape
+    x*cos(θ) - y*sin(θ), y*cos(θ) + x*sin(θ)
+  end
 
-	# center
-	@series begin
-		seriestype := :scatter
-		markersize := 10
-		markercolor := :black
-		annotations := [(0, -0.2, Plots.text("a: $(env.a)", :top))]
-		[0],[0]
-	end
+  # center
+  @series begin
+    seriestype := :scatter
+    markersize := 10
+    markercolor := :black
+    annotations := [(0, -0.2, Plots.text("a: $(env.a)", :top))]
+    [0],[0]
+  end
 end
